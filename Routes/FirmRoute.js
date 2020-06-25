b91b12f0c0f49043e49192eeba520bc501739c43
shassaan <syedhassaan.103@gmail.com>
2020-06-25 15:45:50 +0500

    create put method for firm

--- Routes/FirmRoute.js
+++ Routes/FirmRoute.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router();
-const {createFirm,getAllFirms} = require('../DataBase/BuisnessLayer/FirmVM');
+const {createFirm,getAllFirms,updateFirm} = require('../DataBase/BuisnessLayer/FirmVM');
 router
 .route('/')
 .post(createFirm)
 .get(getAllFirms)
+
+router.route('/:id')
+.put(updateFirm)
 
 module.exports = router;
