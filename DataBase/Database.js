58d2bf108cdedf414ac910d7284560033eb09222
shassaan <syedhassaan.103@gmail.com>
2020-06-28 02:01:01 +0500

    changes

--- DataBase/Database.js
+++ DataBase/Database.js
@@ -3,7 +3,17 @@ let mongoose = require('mongoose');
 const server = '127.0.0.1:27017'; // REPLACE WITH YOUR DB SERVER
 const database ='propertyDb';      // REPLACE WITH YOUR DB NAME
 const password = "hellocluster"
-const url = `mongodb://${server}/${database}`;
+//const url = `mongodb://${server}/${database}`;
+
+//const MongoClient = require('mongodb').MongoClient;
+const url = `mongodb+srv://shassaan:${hellocluster}@cluster0-u7ps2.mongodb.net/${database}?retryWrites=true&w=majority`;
+//const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
 class Database {
   constructor() {
     this._connect()
