8910dcacb2f907008811b5cbcbe2a165321b2f37
Syed Hassaan Ahmed <syedhassaan.103@gmail.com>
2021-02-28 17:51:56 +0500

    [master] changes

--- Routes/FileRoute.js
+++ Routes/FileRoute.js
@@ -1,66 +1,66 @@
 const express = require('express')
-const fileUplaod =  require('express-fileupload')
+const fileUplaod = require('express-fileupload')
 
 const router = express.Router();
 const FileModel = require('../DataBase/Models/File');
 router.use(fileUplaod(
-    
+
 ))
 
-router.post('/Upload', (req, res)=>{
+router.post('/Upload', (req, res) => {
     if (!req.files || Object.keys(req.files).length === 0) {
         return res.status(400).send('No files were uploaded.');
-      }
-    
-      // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-      let uploadedFile = req.files.file;
-    
-      // Use the mv() method to place the file somewhere on your server
-      uploadedFile.mv(__basedir+'/uploads/'+uploadedFile.name.replace(/\s/g, ''), function(err) {
+    }
+
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    let uploadedFile = req.files.file;
+
+    // Use the mv() method to place the file somewhere on your server
+    uploadedFile.mv(__basedir + '/uploads/' + uploadedFile.name.replace(/\s/g, ''), function (err) {
         if (err)
-          return res.status(500).send(err);
-    
-        res.send('/uploads/'+uploadedFile.name.replace(/\s/g, ''));
-      });
+            return res.status(500).send(err);
+
+        res.send('/uploads/' + uploadedFile.name.replace(/\s/g, ''));
+    });
 })
 router.route('/')
-.post((req, res) => {
-    let db = new FileModel(req.body);
-    db.save()
-    .then(doc => {
-        res.send(doc)
+    .post((req, res) => {
+        let db = new FileModel(req.body);
+        db.save()
+            .then(doc => {
+                res.send(doc)
+            })
+            .catch(err => {
+                res.status(400).send(err)
+            })
     })
-    .catch(err => {
-        res.status(400).send(err)
+
+    .get((req, res) => {
+
+        FileModel.find({}, (err, result) => {
+            res.send(result);
+        })
     })
-})
 
-.get((req, res) => {
-    
-    FileModel.find({},(err,result)=>{
-        res.send(result);
+    .delete((req, res) => {
+        FileModel.findByIdAndDelete({ _id: req.body.id })
+            .then(doc => res.sendStatus(200))
+            .catch(err => res.status(400).send(err))
     })
-})
 
-.delete((req, res)=>{
-    FileModel.findByIdAndDelete({_id: req.body.id})
-    .then(doc => res.sendStatus(200))
-    .catch(err => res.status(400).send(err))
-})
+    .put((req, res) => {
 
-.put((req, res) => {
-    
-    FileModel.findOneAndUpdate(
-        {
-            _id: req.body.id
-        },
-        req.body,
-        {
-            new:true
-        }
-    )
-    .then(doc => res.send(doc))
-    .catch(err => res.status(400).send(err))
-})
+        FileModel.findOneAndUpdate(
+            {
+                _id: req.body.id
+            },
+            req.body,
+            {
+                new: true
+            }
+        )
+            .then(doc => res.send(doc))
+            .catch(err => res.status(400).send(err))
+    })
 
 module.exports = router;
